c7e380f205109f7607cbc75d9cee63bfd8e8e641
Ben Morrison <ben@gbmor.dev>
2019-07-14 01:03:14 -0400

    working on default vals for cli flags

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#[macro_use]
+use clap::crate_version;
+use clap::{Arg, App, SubCommand};
+
+fn main() {
+    let args = App::new("rtcoin")
+        .version(crate_version!())
+        .author("Ben Morrison (gbmor) (based on tcoin by login000)")
+        .about("Currency Simulation for the Tildeverse")
+        .arg(Arg::with_name("messages")
+             .short("m")
+             .long("messages")
+             .value_name("[n]")
+             .help("Displays all messages, or last N messages.")
+             .takes_value(true))
+        .arg(Arg::with_name("send")
+             .short("s")
+             .long("send")
+             .value_name("[username] [amount] [message]")
+             .help("Send rtcoin to another user.")
+             .takes_value(true))
+        .subcommand(SubCommand::with_name("on")
+                    .about("Log in to rtcoin"))
+        .subcommand(SubCommand::with_name("off")
+                    .about("Log out of rtcoin"))
+        .subcommand(SubCommand::with_name("init")
+                    .about("Initialize your rtcoin wallet"))
+        .subcommand(SubCommand::with_name("balance")
+                    .about("Retrieve your rtcoin balance"))
+        .get_matches();
+
+    if args.is_present("messages") {
+        let msg_num = args.value_of("messages").unwrap_or("0");
+        let msg_num: u32 = msg_num.trim().parse().unwrap_or(0);
+        println!("Number of messages to display: {}", msg_num);
+    }
+}
