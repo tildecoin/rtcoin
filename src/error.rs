3b755e08103855cdd0b067b9b061a03fd908c6d2
Ben Morrison <ben@gbmor.dev>
2019-07-14 03:54:30 -0400

    local error type for tcoin

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+//
+// rtcoin - Copyright (c) 2019 Ben Morrison (gbmor)
+// See LICENSE file for detailed license information.
+//
+
+use std::error::Error;
+use std::fmt;
+
+// The following struct and its three implementation
+// blocks are for a local error type. Just some boilerplate.
+#[derive(Debug)]
+pub struct TcoinError {
+    details: String,
+}
+
+impl TcoinError {
+    pub fn new(msg: &str) -> TcoinError {
+        TcoinError {
+            details: msg.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for TcoinError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+impl Error for TcoinError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
