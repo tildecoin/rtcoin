dd8422b2173827a36d2ab2d95e86440eaf1b46af
Ben Morrison <ben@gbmor.dev>
2019-07-14 03:55:09 -0400

    user-related structs/funcs
    This is scaffolding at the moment, but will
    hopefully not need too much design change.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,55 @@
+// 
+// rtcoin - Copyright (c) 2019 Ben Morrison (gbmor)
+// See LICENSE file for detailed license information.
+//
+
+// Locals Only
+mod error;
+use error::TcoinError;
+
+// Leaving the fields private to prevent
+// some funny business with the balances
+// or the passwords.
+#[derive(Debug)]
+pub struct User {
+    created: String, // TODO: use the chrono crate for time stuff
+    pass: Vec<u8>,
+    balance: u32,
+    last_login: String,
+}
+
+impl User {
+    pub fn new() -> User {
+        let pass: Vec<u8> = vec![0, 8];
+        User {
+            created: "Placeholder".to_string(),
+            pass: pass,
+            balance: 1000,
+            last_login: "Placeholder".to_string(),
+        }
+    }
+
+    pub fn balance(&self) -> u32 {
+        self.balance
+    }
+
+    pub fn deposit(&mut self, dep: u32) -> Result<(), TcoinError> {
+        if (dep + self.balance) > u32::max_value() {
+            return Err(TcoinError::new("Deposit Overflow"));
+        }
+        self.balance += dep;
+        return Ok(())
+    }
+
+    pub fn withdraw(&mut self, amt: u32) -> Result<(), TcoinError> {
+        if self.balance < amt {
+            return Err(TcoinError::new("Insufficient funds"));
+        }
+        self.balance -= amt;
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
