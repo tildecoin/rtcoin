7c3c1b460d84a14f46a08c03f88da53cee681c80
Ben Morrison <ben@gbmor.dev>
2019-07-14 02:48:36 -0400

    comments added

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
+//
+// rtcoin - Copyright (c) 2019 Ben Morrison (gbmor)
+// See LICENSE file for detailed license information.
+//
+
 use std::thread;
 
 #[macro_use]
-use clap::{crate_version, value_t};
+use clap::{crate_version, value_t}; // the macros
 use clap::{Arg, App, SubCommand};
+
+// TODO: Need to come up with some kind of authentication
+// system for users. And come up with a way to store and
+// retrieve coin balances. The original implementation used
+// flat files.
 
 fn main() {
     print!("\n");
     let args = App::new("rtcoin")
+        // this uses a macro to pull the version
+        // stated in Cargo.toml
         .version(crate_version!())
         .author("Ben Morrison (gbmor) :: based on tcoin, originally by login000")
         .about("Currency Simulation for the Tildeverse")
         .subcommand(SubCommand::with_name("messages")
                     .about("Display all messages, or last N messages")
+                    // if 0 or called but unset, list all messages
                     .arg(Arg::with_name("n")
                          .help("The number of messages to retrieve. Black or 0 means all.")
                          .required(false)))
+        // For the 'send' subcommand, every field is
+        // its own argument. The only optional field
+        // is the third: message.
         .subcommand(SubCommand::with_name("send")
                     .about("Send tcoin to another user. Message is optional.")
                     .arg(Arg::with_name("username")
@@ -29,16 +45,30 @@ fn main() {
                          .help("Optional message to include. Please use quotations.")
                          .required(false)
                          .index(3)))
+        // The "on"/"off" functionality present
+        // in the tcoin API doesn't sit well with
+        // me. I'm going to come up with an
+        // alternative that doesn't present the
+        // same potential issues. Here as a
+        // placeholder, mostly. Like a post-it.
         .subcommand(SubCommand::with_name("on")
                     .about("Log in to rtcoin"))
         .subcommand(SubCommand::with_name("off")
                     .about("Log out of rtcoin"))
         .subcommand(SubCommand::with_name("init")
                     .about("Initialize your rtcoin wallet"))
+        // Balance *should* be the default action,
+        // I think. If init has happened. Else, the
+        // default action should be to init.
         .subcommand(SubCommand::with_name("balance")
                     .about("Retrieve your rtcoin balance"))
         .get_matches();
 
+    // Obviously concurrency isn't strictly
+    // necessary at this point. I'm going to
+    // experiment with some design variations
+    // for authentication that may use extra
+    // threads.
     let next = thread::spawn(move || {
         next_step(args);
     });
@@ -47,10 +77,12 @@ fn main() {
     }
 }
 
-// TODO: Need to come up with some kind of authentication
-// system for users. And come up with a way to store and
-// retrieve coin balances. The original implementation used
-// flat files.
+// TODO: The following functions need to all be moved into
+// other files and imported as libraries. This will allow
+// rtcoin to utilize cargo's built-in unit testing framework.
+// Currently, they are just stubs meant to help me mentally
+// track the program's execution. They will most likely not
+// exist in the near future, replaced with something else.
 
 fn next_step(args: clap::ArgMatches) {
     match args.subcommand() {
