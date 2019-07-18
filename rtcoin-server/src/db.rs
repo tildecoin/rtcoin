5f6435b0be12a051d4bc5132f7d8a549c1551154
Ben Morrison <ben@gbmor.dev>
2019-07-18 01:10:52 -0400

    db connection partially fleshed out, methods returning row iterators

--- rtcoin-server/src/db.rs
+++ rtcoin-server/src/db.rs
@@ -11,6 +11,8 @@ mod crypt;
 
 struct DB {
     conn: sqlite::Connection,
+    user_src_stmt: sqlite::Statement;
+    user_dest_stmt: sqlite::Statement;
     key: Vec<u8>,
 }
 
@@ -26,20 +28,38 @@ impl DB {
         let conn = sqlite::open_with_flags(path, db_flags)
             .expect("Could not open ledger connection");
         
+        conn.execute("CREATE TABLE IF NOT EXISTS ledger (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, timestamp TEXT, source TEXT, destination TEXT, amount REAL, ledger_hash BLOB, receipt_id INTEGER, receipt_hash BLOB)")
+            .unwrap();
+
         let key: Vec<u8> = Vec::new();
+
+        let user_dest_stmt = conn.prepare("SELECT * FROM ledger WHERE dest = ?");
+        let user_src_stmt = conn.prepare("SELECT * FROM ledger WHERE source = ?");
         
         &DB {
             conn,
+            user_src_stmt,
+            user_dest_stmt,
             key,
         }
     }
 
-    fn encrypt() -> Result<(), String> {
+    fn retrieve_dest(&self, user: &str) -> sqlite::Statement {
+        let mut dest_rows = self.user_dest_stmt.clone();
+        dest_rows.bind(user).unwrap()
+    }
+
+    fn retrieve_src(&self, user: &str) -> sqlite::Statement {
+        let mut src_rows = self.user_src_stmt.clone();
+        src_rows.bind(user).unwrap()
+    }
+
+    fn encrypt(&self) -> Result<(), String> {
         crypt(); 
         Ok(())
     }
 
-    fn hmac() -> Result<(), String> {
+    fn hmac(&self) -> Result<(), String> {
         auth(); 
         Ok(())
     }
